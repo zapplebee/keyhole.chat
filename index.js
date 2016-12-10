3585f9b4ceb8c0dca1258d9e9caeceb96c7cf167
Zachary Skalko <zskalko@gmail.com>
2016-12-10 12:20:14 -0600

    Inital branch

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 "use strict"
 const express = require('express');
 const app = express();
-const sassMiddleware = require('node-sass-middleware');
 const path = require('path');
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
@@ -10,27 +9,9 @@ const uuidV4 = require('uuid/v4');
 
 server.listen(80);
 
-
-//pug renderer, index
-app.set('views', './views')
-app.set('view engine', 'pug')
-app.get('/', function (req, res) {
-  res.render('index', {});
-});
 app.use('/script/angular.min.js', express.static(__dirname + '/node_modules/angular/angular.min.js'));
-app.use('/script/socket.io.min.js', express.static(__dirname + '/node_modules/socket.io-client/dist/socket.io.min.js'));
-app.use('/script', express.static(__dirname + '/public'));
-
-//sass
-app.use(sassMiddleware({
-    src: path.join(__dirname, 'style'),
-    dest: path.join(__dirname, 'style'),
-    debug: true,
-    outputStyle: 'extended',
-    prefix: "/style"
-}));
-app.use("/style", express.static(path.join(__dirname, '/style')));
-app.use("/images", express.static(path.join(__dirname, 'images')));
+app.use('/socket.io.min.js', express.static(__dirname + '/node_modules/socket.io-client/dist/socket.io.min.js'));
+app.use('/', express.static(__dirname + '/public'));
 
 var queue = false;
 
--- raw/main.js
+++ raw/main.js
@@ -1,3 +1,108 @@
+var lobby = io.connect('/lobby');
+var room = false;
+var socket;
+
+lobby.on('room',function (roomid) {
+  console.log(roomid);
+  if(room === false){
+    socket = io.connect('/'+roomid);
+    initalizeRoom();
+  }
+  room = roomid;
+});
+
+
+var doEmitPixels = function(){};
+var doEmitTranscript = function(){};
+var dataURL = "";
+
+function initalizeRoom(){
+
+  doEmitPixels = _.throttle(function(){socket.emit('image',dataURL)},100)
+
+  socket.on('image', function (dataURL) {
+    visitor.style['background-image'] = "url('" + dataURL + "')";
+  });
+
+
+  doEmitTranscript = function(text){
+    socket.emit('voice',text);
+    var dialog = document.createElement('p');
+    dialog.innerHTML = text;
+    var fontsize = text.length > 20 ? 0.5 : 1;
+    dialog.style['font-size'] = fontsize + "em"; 
+    me.appendChild(dialog);
+    var t = Math.max(2000,text.length * 100);
+    window.setTimeout(function(){me.removeChild(dialog)},t);
+  }
+
+  socket.on('voice', function (text) {
+    var dialog = document.createElement('p');
+    dialog.innerHTML = text;
+    var fontsize = text.length > 20 ? 0.5 : 1;
+    dialog.style['font-size'] = fontsize + "em"; 
+
+    var utterance = new SpeechSynthesisUtterance(data.text);
+
+    var timeout;
+
+    utterance.onstart = function(){
+      visitor.appendChild(dialog);
+      var t = Math.max(2000,text.length * 100);
+      timeout = window.setTimeout(function(){
+        timeout = null;
+        visitor.removeChild(dialog)
+      },t);
+    }
+
+    utterance.onend = function(){
+      if(timeout){
+        window.clearTimeout(timeout);
+      }
+      
+      visitor.removeChild(dialog);
+    }
+    synth.speak(utterance);
+  });
+}
+
+
+
+var recognizing = false;
+var recognition = new webkitSpeechRecognition();
+recognition.continuous = false;
+recognition.interimResults = false;
+recognition.lang = 'en-US';
+
+recognition.onstart = function() {
+  recognizing = true;
+};
+
+recognition.onerror = function(event) {
+   console.log(event);
+};
+
+recognition.onresult = function(event) {
+  doEmitTranscript(event.results[0][0].transcript);
+};
+
+recognition.onend = function(event){
+  recognizing = false;
+  start();
+}
+
+function start(event) {
+  if (recognizing) { return; } 
+  recognition.start();
+}
+
+var synth = window.speechSynthesis;
+var visitor = document.querySelector('#visitor');
+var me = document.querySelector('#me');
+
+
+
+
 var threshController = document.querySelector("#thresh");
 threshController.value = 100;
 
